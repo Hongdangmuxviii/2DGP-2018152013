70c9d94e2e246ffa5f27047a86e5cf20b6cd6328
sonji3000-cloud <sonji3000@gmail.com>
2026-09-22 15:15:39 +0900

    feat: LEC06 캐릭터 이미지 로드 및 화면 출력 구현

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,13 +1,27 @@
 from pico2d import *
+import math
 
 open_canvas(800, 600)
 charactor = load_image('character.png')
-
+degree =0
 
 def move_circle (): 
     print('CIRCLE')
     clear_canvas()
     charactor.draw(400,300)
+    theta = math.radians(degree)
+    x = 400 + 200 * math.cos(theta)
+    y = 300 + 200 * math.sin(theta)
+    def move_circle():
+     for degree in range(360):
+        theta = math.radians(degree)
+        x = 400 + 200 * math.cos(theta)
+        y = 300 + 200 * math.sin(theta)
+
+        clear_canvas()
+        charactor.draw(x, y)
+        update_canvas()
+        delay(0.01)
     update_canvas()
     pass
 
